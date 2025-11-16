3ade1f056a998d7a1eb4a72dc14a3b89b6190925
alieza <alieza.salim021@gmail.com>
2025-11-16 16:29:53 +0330

    make role model in accounts app

--- accounts/models.py
+++ accounts/models.py
@@ -35,3 +35,15 @@ class User(AbstractUser):
         return self.phone_number
 
 
+class Role(models.Model):
+    """
+    Role model for defining user roles.
+    Supports flexible assignment via ManyToMany in UserProfile.
+    """
+    name = models.CharField(max_length=50, unique=True)
+    description = models.TextField(blank=True)
+
+    def __str__(self):
+        return self.name
+
+
