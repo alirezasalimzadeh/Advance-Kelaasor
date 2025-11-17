95f0b3e10b015cae7715759dd29a3c0b3f9d375c
alieza <alieza.salim021@gmail.com>
2025-11-18 02:17:56 +0330

    add docstring - edit user model

--- accounts/signals.py
+++ accounts/signals.py
@@ -1,12 +1,16 @@
 from django.db.models.signals import post_save
 from django.dispatch import receiver
-from django.conf import settings
 from accounts import models as user_models
+from django.contrib.auth import get_user_model
 
-User = settings.AUTH_USER_MODEL
+User = get_user_model()
 
 
 @receiver(post_save, sender=User)
 def create_user_profile(sender, instance, created, **kwargs):
+    """
+    Signal handler to automatically create a UserProfile
+    whenever a new User instance is created.
+    """
     if created:
         user_models.UserProfile.objects.get_or_create(user=instance)
