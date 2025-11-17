9e46982c1b1c76b858f7b0e9729b32f3e42d8682
alieza <alieza.salim021@gmail.com>
2025-11-17 14:30:37 +0330

    make urls file

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,7 +1,25 @@
-from django.urls import path
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from .views import (
+    SuperAdminUserViewSet,
+    SuperAdminRoleViewSet,
+    SuperAdminProfileViewSet,
+    SupportAdminProfileViewSet,
+    FinancialAdminProfileViewSet,
+    EducationAdminProfileViewSet,
+    UserProfileView,
+)
 
-
+router = DefaultRouter()
+router.register(r'super-admin/users', SuperAdminUserViewSet, basename='super-admin-users')
+router.register(r'super-admin/roles', SuperAdminRoleViewSet, basename='super-admin-roles')
+router.register(r'super-admin/profiles', SuperAdminProfileViewSet, basename='super-admin-profiles')
+router.register(r'support-admin/profiles', SupportAdminProfileViewSet, basename='support-admin-profiles')
+router.register(r'financial-admin/profiles', FinancialAdminProfileViewSet, basename='financial-admin-profiles')
+router.register(r'education-admin/profiles', EducationAdminProfileViewSet, basename='education-admin-profiles')
 
 urlpatterns = [
+    path('profile/', UserProfileView.as_view(), name='user-profile'),
 
+    path('', include(router.urls)),
 ]
